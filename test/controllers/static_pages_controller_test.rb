c5efb00949c15cfed9e7a22d4cede563ff84d52f
Divjot <Divjot65@gmail.com>
2015-06-14 16:03:01 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | ScoreBoard App"
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | ScoreBoard App"
+  end
 end
+
