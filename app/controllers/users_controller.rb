e8489872fbb366b32d343ef4db2c596b3f0e7ab4
Divjot <Divjot65@gmail.com>
2015-06-16 19:07:38 +0000

    Finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new #new action displays the form in the new action for creating new users.
+  end
+  
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      flash[:success] = "Welcome to ScoreBoard"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+private
+
+  def params_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  
 end
+
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "Invalid sign-up submission" do
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: " ",
+                               email: "invalis@user",
+                               password: "123",
+                               password_confirmation: "456" }
+    assert_template 'users/new'
+  end
+end
+  
+  test "valid sign-up submission test" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {name: "Divjot",
+                                    email: "Divjot@gmail.com",
+                                    password: "123456",
+                                    password_confirmation: "123456"}
+    end
+    assert_template 'users/show'
+  end
+end
