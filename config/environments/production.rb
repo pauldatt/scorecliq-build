f08b8e09f481e78a707e2b1c81c6c21c51edbce1
Divjot <Divjot65@gmail.com>
2015-06-28 20:34:59 +0000

    Fixed email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'scoreboard.herokuapp.com'
+  host = 'https://calm-hamlet-4644.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
