a942f22e1eac10a1e11f01680307b2c9567ba205
Divjot Mehton <divjot65@gmail.com>
2016-04-15 19:22:12 +0000

    changed sendgrid production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://calm-hamlet-4644.herokuapp.com'
+  host = 'https://scorecliq.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
