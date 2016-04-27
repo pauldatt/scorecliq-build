6d207697df4a52ee8369e936ef08b791d0468e2b
Divjot Mehton <divjot65@gmail.com>
2016-04-27 16:58:12 +0000

    modified gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'scorecliq-kpauls.c9.io'
+  host = 'scorecliq-dmehton.c9.io'
   config.action_mailer.default_url_options = { host: host }
   
   config.action_controller.include_all_helpers = true
