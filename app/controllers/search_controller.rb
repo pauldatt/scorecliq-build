cbb24b50ce29122cd7617035ae7f07eb70ee049a
Divjot Mehton <divjot65@gmail.com>
2016-06-11 22:18:31 +0000

    fixed minor changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
 
 before_action :logged_in_user, only: :index
 
-    def index
-        @scoreboard_search = Scoreboard.search_by_scoreboard(params[:search]).paginate(:page => params[:page], :per_page => 5)
-    end 
+ def index
+    @scoreboard_search = Scoreboard.search_by_scoreboard(params[:search]).paginate(:page => params[:page], :per_page => 5)
+ end 
 
 end
