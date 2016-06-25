5dc77e2e53423f622f1d1ec59f18711d9d2a0451
Divjot Mehton <divjot65@gmail.com>
2016-06-25 02:01:06 +0000

    untracked db/seeds.rb file

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# end
+
+User.create!(name:  "Kuljinder",
+             email: "div@gmail.ca",
+             password:              "foobar12",
+             password_confirmation: "foobar12",
+             activated: "true",
+             admin: "true")
+
+# 5.times do              
+# Scoreboard.create!(name_of_scoreboard: "scoreboard_abc",
+#                   name_of_organization: "abcdef",
+#                   name_of_activity: "ghijklmn",
+#                   user_id: 1,
+#                   states: "state",
+#                   country: "state",
+#                   cities: "state")
+# end
+
+
