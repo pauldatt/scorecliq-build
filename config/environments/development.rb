a6ceea0cdd269f2fca688b1526f93065225b8dc3
Divjot Mehton <divjot65@gmail.com>
2016-08-18 01:39:15 +0000

    minor changes in dev config file fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'scorecliq-dmehton.c9.io'
+  host = 'scorecliq-dmehton.c9users.io'
   config.action_mailer.default_url_options = { host: host }
   
   config.action_controller.include_all_helpers = true
